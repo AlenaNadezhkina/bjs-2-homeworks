0ee1bc5d4523a3af765451b5cdaec4028c9f7937
AlenaNadezhkina <61390359+AlenaNadezhkina@users.noreply.github.com>
2025-10-08 15:48:40 +0300

    change task.js first work

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  if (a === 0) {
+    if (b === 0) {
+      arr.push(c === 0);
+    } else {
+      arr.push(-c / b)
+    }
+  }
+  const D = b * b - 4 * a * c;
+  if (D < 0) {
+    return false;
+  } else if (D === 0) {
+    const x = -b / (2 * a);
+    arr.push(x);
+  } else if (D > 0) {
+    const x1 = (-b + Math.sqrt(D)) / (2 * a);
+    const x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr.push(x1, x2)
+  }
+  return arr
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  const P = (percent / 100) / 12;
+  const S = amount - contribution;
+  const mouthPay = S * (P + (P / (((1 + P) ** countMonths) - 1)));
+  const allPay = (mouthPay * countMonths).toFixed(2);
+  return Number(allPay);
 }
